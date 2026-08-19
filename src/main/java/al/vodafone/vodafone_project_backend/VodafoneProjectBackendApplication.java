6e89c46b5d497027eb38fdc49bcc37a114fa5109
Orest-Z <orest.zogju@fshnstudent.info>
2026-08-19 18:18:07 +0200

    chore: update gitignore and prepare project files

---
+++ src/main/java/al/vodafone/vodafone_project_backend/VodafoneProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package al.vodafone.vodafone_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodafoneProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodafoneProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/vodafone/vodafone_project_backend/VodafoneProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package al.vodafone.vodafone_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodafoneProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
