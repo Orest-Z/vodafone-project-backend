9467a970ff1b917eea586427734a3ebeff74dc96
Orest-Z <orest.zogju@fshnstudent.info>
2026-08-27 01:29:15 +0200

    made it simpler to now not query for the exact game id code etc since there will only be 1 game mode

--- src/main/java/al/vodafone/vodafone_project_backend/repository/GamePlayRepository.java
+++ src/main/java/al/vodafone/vodafone_project_backend/repository/GamePlayRepository.java
@@ -1,18 +1,15 @@
 package al.vodafone.vodafone_project_backend.repository;
 
+import al.vodafone.vodafone_project_backend.model.DropType;
 import al.vodafone.vodafone_project_backend.model.GamePlay;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Repository
 public interface GamePlayRepository extends JpaRepository<GamePlay, UUID> {
-    boolean existsByTouristIdAndGameId(UUID touristId, UUID gameId);
-
-    @Query("SELECT gp.game.code FROM GamePlay gp WHERE gp.tourist.id = :touristId")
-    List<String> findPlayedGameCodesByTouristId(@Param("touristId") UUID touristId);
+    boolean existsByTouristIdAndGameIdAndPlayedDateAndDropType(
+            UUID touristId, UUID gameId, LocalDate playedDate, DropType dropType);
 }
