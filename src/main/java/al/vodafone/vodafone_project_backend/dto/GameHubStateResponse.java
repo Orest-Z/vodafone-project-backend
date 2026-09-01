29f56936d7f4de6cad6dd255fad8bba68b8de802
Orest-Z <orest.zogju@fshnstudent.info>
2026-09-01 02:08:12 +0200

    added new param for tourist first name which will be used to greet him when he enters the game hub

--- src/main/java/al/vodafone/vodafone_project_backend/dto/GameHubStateResponse.java
+++ src/main/java/al/vodafone/vodafone_project_backend/dto/GameHubStateResponse.java
@@ -3,6 +3,7 @@ package al.vodafone.vodafone_project_backend.dto;
 import java.time.Instant;
 
 public record GameHubStateResponse(
+    String touristFirstName,
     int credits,
     boolean hasPlayedToday,
     boolean dailyClaimAvailable,
